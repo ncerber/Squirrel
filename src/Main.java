dfbde7b5e1b2e3a8e217e8d23c1887f716fd7d1f
ncerber <ncerber@yandex.ru>
2022-03-15 17:42:56 +0500

    Все готово

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Squirrel squirrel = new Squirrel(new int[]{1, -2, 4, 6, 3, -2, -2, -3, -1});
+        System.out.println(squirrel.maxNuts());
+    }
+}
--- src/Squirrel.java
+++ src/Squirrel.java
@@ -9,7 +9,29 @@ public class Squirrel {
         return nuts[index];
     }
 
-    public int maxNuts(){
+    public int maxNuts() {
+        int startIndex = 0;
+        int nutsTotal = 0;
 
+        if (nuts[0] < 0 && nuts.length > 1 && nuts[1] > nuts[0]) startIndex = 1;
+
+        for (int i = startIndex; i < nuts.length; i++) {
+            if (nutsTotal < 0) nutsTotal = 0;
+            if (nuts[i] >= 0) {
+                nutsTotal += nuts[i];
+                continue;
+            }
+
+            if (i == nuts.length - 1) continue;
+
+            if (nuts[i + 1] >= nuts[i]) {
+                nutsTotal += nuts[i + 1];
+                i++;
+            } else {
+                nutsTotal += nuts[i];
+            }
+
+        }
+        return nutsTotal;
     }
 }
