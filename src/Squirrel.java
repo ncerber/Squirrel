fe022f7fa99ac9eb9deeb71dc3fdbddff8642b23
ncerber <ncerber@yandex.ru>
2022-03-15 16:17:20 +0500

    Готов каркас класса Squirrel

---
+++ src/Squirrel.java
@@ -0,0 +1,15 @@
+public class Squirrel {
+    private int[] nuts;
+
+    public Squirrel(int[] nuts) {
+        this.nuts = nuts;
+    }
+
+    public int getNuts(int index) {
+        return nuts[index];
+    }
+
+    public int maxNuts(){
+
+    }
+}
